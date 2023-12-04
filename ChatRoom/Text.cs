30e89c6238f3bf16cbc169633324f523348c4f13
SheepYhangCN <13538773578@163.com>
2023-12-04 21:25:19 +0800

    disable image sending for now

--- ChatRoom/Text.cs
+++ ChatRoom/Text.cs
@@ -16,12 +16,12 @@ public partial class Text : TextEdit
     {
 		if (@event is InputEventKey && @event.IsReleased())
 		{
-			if (DisplayServer.ClipboardHasImage()&&(bool)@event.Get("ctrl_pressed")&&!(bool)@event.Get("shift_pressed")&&!(bool)@event.Get("alt_pressed")&&!(bool)@event.Get("meta_pressed")&&(Key)(long)@event.Get("keycode")==Key.V)
+			/*if (DisplayServer.ClipboardHasImage()&&(bool)@event.Get("ctrl_pressed")&&!(bool)@event.Get("shift_pressed")&&!(bool)@event.Get("alt_pressed")&&!(bool)@event.Get("meta_pressed")&&(Key)(long)@event.Get("keycode")==Key.V)
 			{
 				var texture=ImageTexture.CreateFromImage(DisplayServer.ClipboardGetImage());
 				GetNode<ChatRoom>("../../../").SendImage(Multiplayer.MultiplayerPeer.GetUniqueId(),GetNode<AutoLoad>("/root/AutoLoad").name,Time.GetDatetimeStringFromSystem(false,true),texture);
 				GetNode<ChatRoom>("../../../").Rpc("SendImage",Multiplayer.MultiplayerPeer.GetUniqueId(),GetNode<AutoLoad>("/root/AutoLoad").name,Time.GetDatetimeStringFromSystem(false,true),texture);
-			}
+			}*/
 			if ((bool)@event.Get("shift_pressed"))
 			{
 				if ((Key)(long)@event.Get("keycode")==Key.Enter||(Key)(long)@event.Get("keycode")==Key.KpEnter)
